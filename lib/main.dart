9e457793d931903fbd0f8798048ecfba5f7bb7d2
Tarek Lotfi <tareklotfi789@gmail.com>
2025-11-28 21:24:47 +0200

    feat(splash): ✨ add logo and native splash screen
    Added the DocDoc logo to the app and implemented the native splash screen for a consistent app launch experience.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 // ═══════════════════════════════════════════════════════════════
 
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app/doc_doc_app.dart';
 import 'core/helpers/classes/app_logger.dart';
@@ -12,10 +13,18 @@ import 'core/helpers/functions/initialize_services.dart';
 /// Sets up the environment, initializes services,
 /// configures system UI, and launches the application
 Future<void> main() async {
-  // Initialize Flutter binding before calling native code
-  WidgetsFlutterBinding.ensureInitialized();
+  // The "Preserve" Way (Recommended)
+  // Splash ➔ Still Splash ➔ App UI
+
+  // 1. Capture the Binding
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. PRESERVE: Tell the splash screen to wait for us.
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   Logger.info('Application starting...', 'main');
 
+  // Run heavy async services (Splash screen stays visible during this time)
   await configureSystemUI();
   await initializeServices();
 
